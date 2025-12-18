e79fa4936a9bb4ab7d6dcaa9bb6eafe0c2c14934
Ray Chen <raychen.chen@mail.utoronto.ca>
2025-12-18 13:49:45 -0500

    reform type structure of the parser

--- src/parser/expr.rs
+++ src/parser/expr.rs
@@ -2,31 +2,8 @@ use crate::parser::token::Token;
 
 #[allow(unused)]
 pub enum Expr {
-    Unary(Unary),
-    Binary(Binary),
-    Grouping(Grouping),
-    Literal(Literal),
-}
-
-#[allow(unused)]
-pub struct Unary {
-    operand: Box<Expr>,
-    operator: Token,
-}
-
-#[allow(unused)]
-pub struct Binary {
-    left: Box<Expr>,
-    right: Box<Expr>,
-    operator: Token,
-}
-
-#[allow(unused)]
-pub struct Grouping {
-    expression: Box<Expr>,
-}
-
-#[allow(unused)]
-pub struct Literal {
-    value: Token,
+    Unary(Token, Box<Expr>),
+    Binary(Token, Box<Expr>, Box<Expr>),
+    Grouping(Box<Expr>),
+    Literal(Token),
 }

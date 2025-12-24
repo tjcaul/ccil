d554cfc9ce74e2d13cc46ec8400dac1d6a0811ef
Ray Chen <raychen.chen@mail.utoronto.ca>
2025-12-24 18:26:10 -0500

    change directory

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::fs::read_to_string;
 use ccil::parser::token::Token;
 
 fn main() {
-    let source_file = read_to_string("ccil_source_files/tokenizer_stress_test.ccil").unwrap();
+    let source_file = read_to_string("examples/ccil_source_files/tokenizer_stress_test.ccil").unwrap();
 
     let scan_result = Token::full_scan(&source_file);
 
